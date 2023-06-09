ef64c048fddccfd9411f92f0947d2a9d317ae5f8
Shinsuke <135405632+shinsuke-okudaira@users.noreply.github.com>
2023-06-09 19:55:35 +0900

    問題4_1

---
+++ PracticeExercise_4_1.java
@@ -0,0 +1,17 @@
+package sukkiri_java_4;
+
+public class PracticeExercise_4_1 {
+
+		/*
+		int[] points = new int [4] ; //1問目
+		
+		double[] weights = new double [5] ; //2問目
+		
+		boolean[] answers = new boolean [3] ; //3問目
+		
+		String[] names = new String[3] ; //4問目
+		
+		*/
+	
+
+}
