a32ef25a47ad49aca5683658e8ae9d13d45dd0e6
Shinsuke <135405632+shinsuke-okudaira@users.noreply.github.com>
2023-06-11 10:43:59 +0900

    Ex_9終了

---
+++ thief.java
@@ -0,0 +1,19 @@
+package sukkiri_java_09;
+/*
+ * public class thief{ String name; int hp; int mp;
+ * 
+ * public thief(String name,int hp,int mp){ this.name=name; this.hp=hp;
+ * this.mp=mp; }
+ * 
+ * public thief(String name,int hp){ this(name,5); }
+ * 
+ * public thief(String name){ this(name,40); } }
+ * 
+ * public class main{ public static void heal(int hp){ hp+=10; }
+ * 
+ * public static void heal(Thief thief){ thief.hp+=10; }
+ * 
+ * public static void main(String[] args){ int baseHp=25; Thief t=new
+ * Thief("アサカ",baseHp); heal(baseHp); System.out.println(baseHp+":"+t.hp);
+ * System.out.println(baseHp+":"+t.hp); } }
+ */
